b94fbf1ba00a9f7ded2666f3f7d554ddd4367b44
minseok <msj6872@gmail.com>
2024-02-18 21:29:00 +0900

    week4

--- src/week04/bracketCheck.java
+++ src/week04/bracketCheck.java
@@ -1,7 +1,34 @@
 package week04;
 
-public class bracketChecker {
+import java.util.Stack;
+
+public class bracketCheck {
     public static void main(String[] args) {
-        
+        String example1 = "((())())";
+        String example2 = "(()()))(";
+        String example3 = "((())";
+        String example4 = "()())";
+
+        System.out.println(isCorrect(example1));
+        System.out.println(isCorrect(example2));
+        System.out.println(isCorrect(example3));
+        System.out.println(isCorrect(example4));
+    }
+
+    public static boolean isCorrect(String brackets) {
+        Stack<Character> bracketStack = new Stack<>();
+
+        for (char bracket : brackets.toCharArray()) {
+            if (bracket == '(') {
+                bracketStack.push('(');
+            } else if (bracket == ')') {
+                if (bracketStack.isEmpty()) {
+                    return false;
+                } else {
+                    bracketStack.pop();
+                }
+            }
+        }
+        return bracketStack.isEmpty();
     }
 }
